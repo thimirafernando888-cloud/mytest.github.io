9caf0344f873e8fd66e27ebdea8d763df817fad9
thimira fernando <thimirafernando888@gmail.com>
2026-09-04 10:04:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// This function changes the heading text when you click button 1
+function changeText() {
+    const heading = document.querySelector("h1");
+    heading.textContent = "You clicked the button! Magic!";
+}
